1de2e007a184cff9c6565a9c16b2e1357ade0ed6
root <root@DESKTOP-RD56JUJ.localdomain>
2022-09-29 20:01:11 +0200

    Opgave 7 færdig

--- jollyJumper.c
+++ jollyJumper.c
@@ -1,11 +1,50 @@
 #include "jollyjumper.h"
 #include <stdbool.h>
 #include <malloc.h>
-
-
+#include <stdlib.h>
+#include <stdio.h>
 
 int isJollyJumber(const int seq[], int size) {
-/*post: answer to exercise 7.a*/
-    return 1;
+
+	//Printer hvor mange differancer der er
+	printf("\n%d\n", size-1);
+
+	//Bool til at kontroller om der er flere differancer med samme værdi
+	bool  diffs_found[size - 1];
+
+	//Sætter alle værdier i arrayet til false
+	for(int i = 0; i < size  - 1; i++){
+		diffs_found[i] = false;
+	}
+
+	//Finder differancen fra det nuværende tal og det tidligere tal i arrayet
+	for(int i = 1; i < size; i++){
+		int diff = abs(seq[i] - seq[i-1]);
+		printf("%d ", diff);
+		/*Hvis differancen er større end arrayet så kan det aldrig blive en Jolly Jumper
+		Hvis differancen er 0 kan arrayet heller ikke være en Jolly Jumper*/
+		if(diff >= size || diff == 0){
+			return 0;
+		}
+
+
+		/*Tjekker om diff værdien er gentagende, ved at tjekke om det der svarer til
+		værdiens plads i arrayet er falsk og hvis ja, er det nu true */
+		if(diffs_found[diff - 1] == false){
+			diffs_found[diff - 1] = true;
+		}
+
+	}
+
+	/*Tjekker bool arrayet om der er nogle falske pladser,
+	Da hvis der er det, så er der to diff med samme værdi
+	og arrayet er ikke en Jolly Jumper*/
+	for(int i = 0; i < size - 1; i++){
+		if(diffs_found[i] == false){
+			return 0;
+		}
+	}
+
+	return 1;
 }
 
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include "circle.h"
 #include "jollyjumper.h"
+#include <assert.h>
 
 void printCircle(circle c) {
 	printf("\np.x: %d, p.y: %d, r: %d\n", c.p.x, c.p.y, c.r);
@@ -30,14 +31,28 @@ int main(void) {
 	int n; /*number of numbers to read*/
 	/*readin n and check that is is OK*/
 
+	//Bruger intaster længde på array
+	printf("\nSkriv længden på dit array:\n");
+	scanf("%d", &n);
+
+	/*Hvis arrayets størrelse er lig eller under 1
+	kan der ikke findes nogle diffenrancer*/
+	assert(!(n <= 1);
+
 	int *numbers = malloc(sizeof(int) * n); /*the numbers read*/
+
+	//Bruger intaster værdier af arrayet
+	printf("Skriv dine værdier i arrayet:\n");
+	for(int i = 0; i < n; i++){
+		scanf("%d", &numbers[i]);
+	}
 
 	/*readin the n numbers in the array numbers*/
 
 	if (isJollyJumber(numbers, n)) {
-		printf("it is a Jolly Jumper");
+		printf("\nit is a Jolly Jumper\n");
 	}else{
-		printf("not a Jolly Jumper");
+		printf("\nnot a Jolly Jumper\n");
 	}
 
 	return 0;
